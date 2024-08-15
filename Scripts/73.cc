908628d6801ac2b8e25c9cc8596091678ee53dfd
pandacase <2227119810@qq.com>
2024-08-15 21:41:15 +0800

    add note to problem 73, a infunny problem

--- Scripts/73.cc
+++ Scripts/73.cc
@@ -3,64 +3,101 @@
 #include <iostream>
 
 //! @brief 
-//! 给定一个n×n的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转90度。
-//! 你必须在 原地 旋转图像。
+//! 给定一个mxn的矩阵，如果一个元素为0，则将其所在行和列的所有元素都设为0。
+//! 请使用原地算法并尝试使用常数的额外空间而不仅限于 O(m+n)。
 //! 
 //! @details 
-//! n == matrix.length == matrix[i].length
-//! 1 <= n <= 20
-//! -1000 <= matrix[i][j] <= 1000
+//! m == matrix.length
+//! n == matrix[0].length
+//! 1 <= m, n <= 200
+//! -231 <= matrix[i][j] <= 231 - 1
 //! 
 //! @example 
-//! input:  matrix = [[1,2,3],[4,5,6],[7,8,9]]
-//! output: [[7,4,1],[8,5,2],[9,6,3]]
+//! input:  matrix = [[1,1,1],[1,0,1],[1,1,1]]
+//! output: [[1,0,1],[0,0,0],[1,0,1]]
 //! 
-//! input:  matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-//! output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+//! input:  matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+//! output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 //! 
 //! @note
-//! n = 4
-//! lIdx = 0
-//! 0,0 -> 0,3 -> 3,3 -> 3,0
-//! 0,1 -> 1,3 -> 3,2 -> 2,0
-//! 0,2 -> 2,3 -> 3,1 -> 1,0
-//! ------------------------
-//! lIdx = 1
-//! 1,1 -> 1,2 -> 2,2 -> 2,1
+//! O(m+n)额外空间的做法是，使用两个集合标记出现过0的行和列，再遍历一遍置为0
+//! 实际上可以使用第一行和第一列（除了0,0）来充当这个标记集合，
+//! 然后使用额外 2 个 bool 来标记第一行/第一列是否出现了0.
+//! 如果出现了，在最后把 第一行/第一列 全设为 0 即可，未出现则不用管
+//! （因为如果某一位置被标记成了0，本来也会在第三步设0的时候被传染为0，
+//!   没有被标记为0的，那这个数也不需要修改，所以只要 false 就不用管）
+//!
+//! 极致变态的方法：(Just for fun)
+//! 注意到第三步置0的时候，0,0 是不需要用作标记的
+//! 所以这一位置可以用来存储第一行是否出现了 0，然后就只需要额外的另一个 bool
+//! 来标记第一列是否出现了 0 了。
+//! 不过最后第四步的时候千万记得要先拿 matrix[0][0] 来判断第一行，
+//! 防止被第一列置 0 的时候污染到了。
 class Solution {
 public:
-  void 
-  rotate(std::vector<std::vector<int>>& matrix) 
-  {
-    int n = matrix.size(), layer = n / 2;
-    for (int lIdx = 0; lIdx < layer; ++lIdx) {
-      for (int i = lIdx; i < n - 1 - lIdx; ++i) {
-        // get 4 corner.
-        int& topLeft     = matrix[lIdx][i];
-        int& topRight    = matrix[i][(n - 1) - lIdx];
-        int& bottomRight = matrix[(n - 1) - lIdx][(n - 1) - i];
-        int& bottomLeft  = matrix[(n - 1) - i][lIdx];
-        
-        // rotate 4 corner.
-        int tmp     = bottomLeft;
-        bottomLeft  = bottomRight;
-        bottomRight = topRight;
-        topRight    = topLeft;
-        topLeft     = tmp;
+    void 
+    setZeroes(std::vector<std::vector<int>>& matrix) 
+    {
+      int m = matrix.size(), n = matrix[0].size();
+
+      // Step 1: record if col-0 or row-0 has zero
+      bool zeroFlagCol = false;
+      for (int i = 0; i < m; ++i) {
+        if (matrix[i][0] == 0) {
+          zeroFlagCol = true;
+          break;
+        }
+      }
+      bool zeroFlagRow = false;
+      for (int i = 0; i < n; ++i) {
+        if (matrix[0][i] == 0) {
+          zeroFlagRow = true;
+          break;
+        }
+      }
+
+      // Step 2: tag
+      for (int i = 1; i < m; ++i) {
+        for (int j = 1; j < n; ++j) {
+          if (matrix[i][j] == 0) {
+            matrix[i][0] = 0;
+            matrix[0][j] = 0;
+          }
+        }
+      }
+
+      // Step 3: set zero
+      for (int i = 1; i < m; ++i) {
+        for (int j = 1; j < n; ++j) {
+          if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+            matrix[i][j] = 0;
+          }
+        }
+      }
+
+      // Step 4: if needed, set zero for col-0 or row-0
+      if (zeroFlagCol) {
+        for (int i = 0; i < m; ++i) {
+          matrix[i][0] = 0;
+        }
+      }
+      if (zeroFlagRow) {
+        for (int i = 0; i < n; ++i) {
+          matrix[0][i] = 0;
+        }
       }
     }
-  }
 };
 
 int main() {
   Solution sol;
   std::vector<std::vector<int>> matrix{
-    {5,1,9,11},
-    {2,4,8,10},
-    {13,3,6,7},
-    {15,14,12,16}
+    {1,  2,  3,  4},
+    {5,  0,  7,  8},
+    {0,  10, 11, 12},
+    {13, 14, 15, 0}
   };
-  sol.rotate(matrix);
+  sol.setZeroes(matrix);
 
   //! 0  
   // std::cout << ans << std::endl;

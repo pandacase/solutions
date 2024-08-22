65411ecb6871049ef4215ad11b37c94a7bde0b64
pandacase <2227119810@qq.com>
2024-08-22 14:55:23 +0800

    complete problem 206

--- Scripts/206.cc
+++ Scripts/206.cc
@@ -23,17 +23,17 @@ struct ListNode {
 //! 
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-      ListNode* prev = nullptr;
-      ListNode* curr = head;
-      while (curr != nullptr) {
-        ListNode* next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-      }
-      return prev;
+  ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+    while (curr != nullptr) {
+      ListNode* next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
     }
+    return prev;
+  }
 };
 
 

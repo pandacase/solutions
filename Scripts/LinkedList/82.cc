5f1cb26c3c7cf5134bc4227014723e91ddeb078f
pandacase <2227119810@qq.com>
2024-08-27 05:08:08 +0800

    complete proble 82

--- Scripts/LinkedList/82.cc
+++ Scripts/LinkedList/82.cc
@@ -58,7 +58,7 @@ public:
 int main() {
   Solution sol;
 
-  ListNode* l1 = new ListNode(1, new ListNode(2, new ListNode(2)));
+  ListNode* l1 = new ListNode(2, new ListNode(2, new ListNode(3)));
 
   auto head = sol.deleteDuplicates(l1);
 
